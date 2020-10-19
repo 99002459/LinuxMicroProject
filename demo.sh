0d0271dcd1ccb7d7e36b1f78e934fbe1559b1f99
99002459 <71481634+99002459@users.noreply.github.com>
2020-10-19 08:36:36 +0530

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,5 +1,5 @@
 #! BIN/BASH
-echo "Name", "Email", "Repo Link", "Clone Status", "Build Status", "CPPcheck errors", "Valgrind Pass cases" > log.csv 
+echo "Name", "Email", "Repo Link", "Clone Status", "Build Status", "CPPcheck errors", "Valgrind Pass cases" > Results.csv 
 while IFS=, read -r name email repo; do
 
   [[ "$name" != "Name" ]] && echo "$name"
@@ -12,22 +12,22 @@ while IFS=, read -r name email repo; do
       CLONE_STAT="Clone Failed"
     fi
     reponame=`echo $repo | cut -d'/' -f5`
-    build=`find $reponame -name Makefile -exec dirname {} \;`
-    make -C $build
-    make run -C $build
+    BUILD=`find $reponame -name Makefile -exec dirname {} \;`
+    make -C $BUILD
+    make run -C $BUILD
     if [ "$?" == 0 ]; then
       BUILD_SUCC="Build Successful"
     else
       BUILD_SUCC="Build Failed"
     fi
-    err=`cppcheck $reponame | grep 'error' | wc -l`        
-    make test -C $build
-    FIN_VAL=`find "$build" -name "Test*.out"`
+    ERR=`cppcheck $reponame | grep 'error' | wc -l`        
+    make test -C $BUILD
+    FIN_VAL=`find "$BUILD" -name "Test*.out"`
     echo "dir = $FIN_VAL"
     valgrind "./$FIN_VAL" 2> valgrinr.csv
     VALG=`grep "ERROR SUMMARY" valgrinr.csv`
     echo "ERR = ${VALG:25:1}"
-    echo "$name, $email, $repo, $CLONE_STAT, $BUILD_SUCC, $err, ${VALG:25:1}" > log.csv      
+    echo "$name, $email, $repo, $CLONE_STAT, $BUILD_SUCC, $ERR, ${VALG:25:1}" > Results.csv      
   fi
     
 done < Input.csv
